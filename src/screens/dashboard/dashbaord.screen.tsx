57d83219d11d41fa3f44ae870ce004871356fdd8
Karan Rawal <kr14nov@outlook.com>
2020-08-30 15:02:46 +0530

    Refactored dashboard screen.

--- src/screens/dashboard/dashbaord.screen.tsx
+++ src/screens/dashboard/dashbaord.screen.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {View, Button, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {ROUTE_USER_DETAILS} from '../user-details';
 import {ROUTE_USERS} from '../users';
@@ -7,28 +8,30 @@ import {AppRoutesParamsList} from '../../app.routes';
 
 export const ROUTE_DASHBOARD = 'dashboard';
 
-export interface DashboardScreenProps {
-  navigation: StackNavigationProp<AppRoutesParamsList, typeof ROUTE_DASHBOARD>;
-}
+export interface DashboardScreenProps {}
+type DashboardNavigationProp = StackNavigationProp<
+  AppRoutesParamsList,
+  typeof ROUTE_DASHBOARD
+>;
 
-export class DashboardScreen extends React.Component<DashboardScreenProps> {
-  goToUserDetails = () => {
-    this.props.navigation.push(ROUTE_USER_DETAILS, {
+export const DashboardScreen: React.SFC<DashboardScreenProps> = ({}) => {
+  const navigation = useNavigation<DashboardNavigationProp>();
+
+  const goToUserDetails = useCallback(() => {
+    navigation.push(ROUTE_USER_DETAILS, {
       userId: 1,
     });
-  };
+  }, [navigation]);
 
-  goToUsers = () => {
-    this.props.navigation.push(ROUTE_USERS);
-  };
+  const goToUsers = useCallback(() => {
+    navigation.push(ROUTE_USERS);
+  }, [navigation]);
 
-  render() {
-    return (
-      <View>
-        <Text>Dashboard Screen</Text>
-        <Button title="User Details" onPress={this.goToUserDetails} />
-        <Button title="Users" onPress={this.goToUsers} />
-      </View>
-    );
-  }
-}
+  return (
+    <View>
+      <Text>Dashboard Screen</Text>
+      <Button title="User Details" onPress={goToUserDetails} />
+      <Button title="Users" onPress={goToUsers} />
+    </View>
+  );
+};
