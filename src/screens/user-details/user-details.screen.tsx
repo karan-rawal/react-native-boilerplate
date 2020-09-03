a1dc3a8dca0fad485cd5efb9765ade58f2fd17c8
Karan Rawal <kr14nov@outlook.com>
2020-09-03 18:42:10 +0530

    Converted to function based component.

--- src/screens/user-details/user-details.screen.tsx
+++ src/screens/user-details/user-details.screen.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import {connect, MapStateToProps, MapDispatchToProps} from 'react-redux';
+import {useEffect, useCallback} from 'react';
+import {useRoute} from '@react-navigation/native';
+import {useSelector, useDispatch} from 'react-redux';
 import {RouteProp} from '@react-navigation/native';
 import {AppRoutesParamsList} from '../../app.routes';
 import {UserState, fetchUserAction} from '../../redux/user';
@@ -8,38 +10,61 @@ import UserDetailsComponent from './user-details.component';
 
 export const ROUTE_USER_DETAILS = 'user-details';
 
-export interface UserDetailsScreenProps {
-  userState: UserState;
-  fetchUser: typeof fetchUserAction;
-  route: RouteProp<AppRoutesParamsList, typeof ROUTE_USER_DETAILS>;
-}
+export interface UserDetailsScreenProps {}
+type UserDetailsRouteProp = RouteProp<
+  AppRoutesParamsList,
+  typeof ROUTE_USER_DETAILS
+>;
 
-class Component extends React.Component<UserDetailsScreenProps> {
-  constructor(props: UserDetailsScreenProps) {
-    super(props);
-    this.props.fetchUser(props.route.params.userId);
-  }
-
-  render() {
-    return <UserDetailsComponent userState={this.props.userState} />;
-  }
-}
-
-const mapStateToProps: MapStateToProps<
-  {},
-  UserDetailsScreenProps,
-  ReducerState
-> = (state) => {
-  return {
-    userState: state.userState,
-  };
+const userStateSelector = (state: ReducerState): UserState => {
+  return state.userState;
 };
 
-const mapDispatchToProps: MapDispatchToProps<{}, UserDetailsScreenProps> = {
-  fetchUser: fetchUserAction,
+export const UserDetailsScreen: React.SFC<UserDetailsScreenProps> = ({}: UserDetailsScreenProps) => {
+  const userState = useSelector<ReducerState, UserState>(userStateSelector);
+  const dispatch = useDispatch();
+  const route = useRoute<UserDetailsRouteProp>();
+
+  const fetchUser = useCallback(
+    (userId: number) => {
+      dispatch(fetchUserAction(userId));
+    },
+    [dispatch],
+  );
+
+  useEffect(() => {
+    fetchUser(route.params.userId);
+  }, [route.params.userId, fetchUser]);
+
+  return <UserDetailsComponent userState={userState} />;
 };
 
-export const UserDetailsScreen = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Component);
+// class Component extends React.Component<UserDetailsScreenProps> {
+//   constructor(props: UserDetailsScreenProps) {
+//     super(props);
+//     this.props.fetchUser(props.route.params.userId);
+//   }
+
+//   render() {
+//     return <UserDetailsComponent userState={this.props.userState} />;
+//   }
+// }
+
+// const mapStateToProps: MapStateToProps<
+//   {},
+//   UserDetailsScreenProps,
+//   ReducerState
+// > = (state) => {
+//   return {
+//     userState: state.userState,
+//   };
+// };
+
+// const mapDispatchToProps: MapDispatchToProps<{}, UserDetailsScreenProps> = {
+//   fetchUser: fetchUserAction,
+// };
+
+// export const UserDetailsScreen = connect(
+//   mapStateToProps,
+//   mapDispatchToProps,
+// )(Component);
