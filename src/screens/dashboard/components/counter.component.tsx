6765883cf45b6f0709f428bd1fd2fd97cf019e37
Karan Rawal <kr14nov@outlook.com>
2020-09-15 15:42:03 +0530

    Performance talk.

--- src/screens/dashboard/components/counter.component.tsx
+++ src/screens/dashboard/components/counter.component.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
 import {View, Text, Button, TextInput} from 'react-native';
 
+const calculateInitialCount = () => {
+  console.log('Something heavy is going on in here.');
+  return 0;
+};
+
 export const CounterComponent: React.SFC<{}> = () => {
   let previousCount = React.useRef(0);
   const textInputRef = React.useRef<TextInput>(null);
   const textInputCallbackRef = React.useCallback((ref: TextInput) => {
     ref?.blur();
   }, []);
+  const [count, setCount] = React.useState(() => calculateInitialCount());
 
-  const [count, setCount] = React.useState(0);
-
-  const incrementCount = () => {
-    previousCount.current = count;
-    setCount(count + 1);
-  };
+  const incrementCount = React.useCallback(() => {
+    setCount((prevState) => {
+      previousCount.current = prevState;
+      return prevState + 1;
+    });
+  }, []);
 
   const doFocus = () => {
     if (textInputRef.current) {
