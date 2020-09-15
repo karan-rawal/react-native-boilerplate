d7d5e253adfb09d1d13b2a8a5141644ed43e1ce2
Karan Rawal <kr14nov@outlook.com>
2020-09-15 13:00:15 +0530

    use ref 1

--- src/screens/dashboard/components/counter.component.tsx
+++ src/screens/dashboard/components/counter.component.tsx
@@ -1,31 +1,20 @@
 import * as React from 'react';
 import {View, Text, Button} from 'react-native';
 
-export interface CounterComponentState {
-  count: number;
-}
+export const CounterComponent: React.SFC<{}> = () => {
+  let previousCount = React.useRef(0);
+  const [count, setCount] = React.useState(0);
 
-export class CounterComponent extends React.Component<
-  {},
-  CounterComponentState
-> {
-  previousCount = 0;
-  state = {count: 0};
-
-  incrementCount = () => {
-    this.previousCount = this.state.count;
-    this.setState({
-      count: this.state.count + 1,
-    });
+  const incrementCount = () => {
+    previousCount.current = count;
+    setCount(count + 1);
   };
 
-  render() {
-    return (
-      <View>
-        <Text>Current Count: {this.state.count}</Text>
-        <Text>Previous Count: {this.previousCount}</Text>
-        <Button title="increment count" onPress={this.incrementCount} />
-      </View>
-    );
-  }
-}
+  return (
+    <View>
+      <Text>Current Count: {count}</Text>
+      <Text>Previous Count: {previousCount.current}</Text>
+      <Button title="increment count" onPress={incrementCount} />
+    </View>
+  );
+};
